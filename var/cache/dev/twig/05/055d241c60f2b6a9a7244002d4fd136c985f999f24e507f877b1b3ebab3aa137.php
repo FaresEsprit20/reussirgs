3e6752deba56a033483d57ba31dfef3a01e47750
FaresEsprit20 <63284587+FaresEsprit20@users.noreply.github.com>
2020-08-11 13:15:23 +0100

    fourth commit

---
+++ var/cache/dev/twig/05/055d241c60f2b6a9a7244002d4fd136c985f999f24e507f877b1b3ebab3aa137.php
@@ -0,0 +1,370 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* @Gs/Default/espace/solides.html.twig */
+class __TwigTemplate_8ceb9d58a798751b22321d2eb6221ee25269275421d99d63b533122c1b91154f extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'main' => [$this, 'block_main'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "@Gs/Default/espace/solides.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "@Gs/Default/espace/solides.html.twig"));
+
+        $this->parent = $this->loadTemplate("base.html.twig", "@Gs/Default/espace/solides.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 2
+    public function block_title($context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        // line 3
+        echo "  _ Les Solides
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 6
+    public function block_main($context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "main"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "main"));
+
+        // line 7
+        echo "
+<style>
+
+.selectedd{
+border:2px inset silver;
+        }
+
+#valider {
+margin-top: 50px;
+margin-left: 100px;
+}
+
+.small {
+    width: 92px !important;
+}
+
+.btn-lg{
+  width: 32px !important;
+}
+
+.inline-block{
+width: 300px;
+height: 150px;
+}
+
+img:hover {
+  opacity: 0.5;
+}
+
+</style>
+
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 40
+    public function block_body($context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 41
+        echo "  <!-- Tips Triks Start -->
+  <div class=\"tips-triks-area tips-padding\">
+    <div class=\"container\">
+      <!-- Section-tittle -->
+      <div class=\"row d-flex justify-content-center\">
+        <div class=\"col-lg-6 col-md-8 pr-0\">
+          <div class=\"section-tittle text-center\">
+            <h5>
+              Reconnaitre les solides
+            </h5>
+          </div>
+        </div>
+      </div>
+      <br><br>
+    <div class=\"d-inline-flex mb-3\">
+    <p class=\"sample-text \">Quelle figure représente une sphère ?<p>
+    </div>
+<br>
+      <div  class=\"banner\">
+    <img  class=\"img\" src =\"";
+        // line 60
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("assets/images/cube.png"), "html", null, true);
+        echo "\">
+    <img  class=\"img\" src =\"";
+        // line 61
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("assets/images/sphere.png"), "html", null, true);
+        echo "\">
+     </div>
+    </div>
+  </div>
+  <br>
+<div class=\"block\">
+       <div class=\"span2\">
+\t<p ><button id=\"valider\" class=\"genric-btn primary small \">valider</button></p>
+  </div>
+  </div>
+  <div id=\"response\">
+   <div id=\"html\"></div>         
+       <div class=\"block text-center\"><button id=\"ok\"  hidden=\"true\" class=\"genric-btn primary \">ok</button></div>
+  </div>
+ 
+  <!-- Tips Triks End -->
+
+  <script>
+    jQuery( document ).ready(function( \$ ) {
+      //identification de l'image sélectionné et trouver l'index de l'image de la bonne réponse
+        \$(\"#valider\").click(function () {
+          if(!\$('.img').hasClass('selectedd')){
+            alert(\"vide! choisir une option\");
+          }else{
+            if(\$('.selectedd').index() == 1){ 
+           \$(\"#html\").html('<div class=\"section-tittle text-center success\"><h2 style=\"color:green\">Bravo!</h2>\t');
+         document.getElementById('response').scrollIntoView();
+            }else{
+              \$(\"#html\").html('<div class=\"section-tittle text-center danger\"><h2 style=\"color:red\">Echec! la bonne réponse est la deuxième image</h2>\t');
+            document.getElementById('response').scrollIntoView();
+            }       
+             \$(this).prop(\"disabled\",true);
+             \$(\"#ok\").attr(\"hidden\",false);
+          }
+        });
+    
+ \$(\"#ok\").click(function () {
+          \$(\"#response\").text(\"\");
+        });
+    });
+
+\$(function(){
+  var ff;    
+    //pour  selectionner les images et implémenter le compteur
+   \$('.img').on('click', function(evt) {
+        ff = \$(this);        
+        \$('.img').removeClass('selectedd');
+        ff.addClass('selectedd');
+        evt.stopPropagation();
+        //alert(\"\"+ff.index());
+        
+    });   
+    
+});
+ 
+</script>
+
+
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@Gs/Default/espace/solides.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  157 => 61,  153 => 60,  132 => 41,  123 => 40,  82 => 7,  73 => 6,  62 => 3,  53 => 2,  31 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% extends 'base.html.twig' %}
+{% block title %}
+  _ Les Solides
+{% endblock %}
+
+{% block main %}
+
+<style>
+
+.selectedd{
+border:2px inset silver;
+        }
+
+#valider {
+margin-top: 50px;
+margin-left: 100px;
+}
+
+.small {
+    width: 92px !important;
+}
+
+.btn-lg{
+  width: 32px !important;
+}
+
+.inline-block{
+width: 300px;
+height: 150px;
+}
+
+img:hover {
+  opacity: 0.5;
+}
+
+</style>
+
+{% endblock %}
+
+{% block body %}
+  <!-- Tips Triks Start -->
+  <div class=\"tips-triks-area tips-padding\">
+    <div class=\"container\">
+      <!-- Section-tittle -->
+      <div class=\"row d-flex justify-content-center\">
+        <div class=\"col-lg-6 col-md-8 pr-0\">
+          <div class=\"section-tittle text-center\">
+            <h5>
+              Reconnaitre les solides
+            </h5>
+          </div>
+        </div>
+      </div>
+      <br><br>
+    <div class=\"d-inline-flex mb-3\">
+    <p class=\"sample-text \">Quelle figure représente une sphère ?<p>
+    </div>
+<br>
+      <div  class=\"banner\">
+    <img  class=\"img\" src =\"{{ asset('assets/images/cube.png') }}\">
+    <img  class=\"img\" src =\"{{ asset('assets/images/sphere.png') }}\">
+     </div>
+    </div>
+  </div>
+  <br>
+<div class=\"block\">
+       <div class=\"span2\">
+\t<p ><button id=\"valider\" class=\"genric-btn primary small \">valider</button></p>
+  </div>
+  </div>
+  <div id=\"response\">
+   <div id=\"html\"></div>         
+       <div class=\"block text-center\"><button id=\"ok\"  hidden=\"true\" class=\"genric-btn primary \">ok</button></div>
+  </div>
+ 
+  <!-- Tips Triks End -->
+
+  <script>
+    jQuery( document ).ready(function( \$ ) {
+      //identification de l'image sélectionné et trouver l'index de l'image de la bonne réponse
+        \$(\"#valider\").click(function () {
+          if(!\$('.img').hasClass('selectedd')){
+            alert(\"vide! choisir une option\");
+          }else{
+            if(\$('.selectedd').index() == 1){ 
+           \$(\"#html\").html('<div class=\"section-tittle text-center success\"><h2 style=\"color:green\">Bravo!</h2>\t');
+         document.getElementById('response').scrollIntoView();
+            }else{
+              \$(\"#html\").html('<div class=\"section-tittle text-center danger\"><h2 style=\"color:red\">Echec! la bonne réponse est la deuxième image</h2>\t');
+            document.getElementById('response').scrollIntoView();
+            }       
+             \$(this).prop(\"disabled\",true);
+             \$(\"#ok\").attr(\"hidden\",false);
+          }
+        });
+    
+ \$(\"#ok\").click(function () {
+          \$(\"#response\").text(\"\");
+        });
+    });
+
+\$(function(){
+  var ff;    
+    //pour  selectionner les images et implémenter le compteur
+   \$('.img').on('click', function(evt) {
+        ff = \$(this);        
+        \$('.img').removeClass('selectedd');
+        ff.addClass('selectedd');
+        evt.stopPropagation();
+        //alert(\"\"+ff.index());
+        
+    });   
+    
+});
+ 
+</script>
+
+
+{% endblock %}", "@Gs/Default/espace/solides.html.twig", "C:\\wamp64\\www\\PFA\\src\\GsBundle\\Resources\\views\\Default\\espace\\solides.html.twig");
+    }
+}
---
+++ var/cache/dev/twig/d5/d5858afa6ecc6b0305b9fad796f582005c17ded566182536413c58ff8dfba977.php
@@ -0,0 +1,350 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* @Gs/Default/grandeurs/comparerlongueurs.html.twig */
+class __TwigTemplate_9c893296da7566f0a092daa109e05cd0bbe586649d13594a6f8ad2c7c7cff869 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->blocks = [
+            'title' => [$this, 'block_title'],
+            'main' => [$this, 'block_main'],
+            'body' => [$this, 'block_body'],
+        ];
+    }
+
+    protected function doGetParent(array $context)
+    {
+        // line 1
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "@Gs/Default/grandeurs/comparerlongueurs.html.twig"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "@Gs/Default/grandeurs/comparerlongueurs.html.twig"));
+
+        $this->parent = $this->loadTemplate("base.html.twig", "@Gs/Default/grandeurs/comparerlongueurs.html.twig", 1);
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    // line 2
+    public function block_title($context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "title"));
+
+        // line 3
+        echo "  _ Comparer Longueurs
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 6
+    public function block_main($context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "main"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "main"));
+
+        // line 7
+        echo "
+<style>
+.selectedd{
+border:2px inset silver;
+        }
+.small {
+    width: 92px !important;
+}
+.btn-lg{
+  width: 32px !important;
+}
+.inline-block{
+width: 300px;
+height: 150px;
+}
+img:hover {
+  opacity: 0.5;
+}
+</style>
+
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    // line 29
+    public function block_body($context, array $blocks = [])
+    {
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->env->getExtension("Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension");
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->enter($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->env->getExtension("Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension");
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
+
+        // line 30
+        echo "  <!-- Tips Triks Start -->
+  <div class=\"tips-triks-area tips-padding\">
+    <div class=\"container\">
+      <!-- Section-tittle -->
+      <div class=\"row d-flex justify-content-center\">
+        <div class=\"col-lg-6 col-md-8 pr-0\">
+          <div class=\"section-tittle text-center\">
+            <h5>
+              Comparer Longueurs
+            </h5>
+          </div>
+        </div>
+      </div>
+      <br><br>
+    <div class=\"d-inline-flex mb-3\">
+    <p class=\"sample-text \">Quelle est le voiture <b>la plus longue</b>&nbsp;?<p>
+    </div>
+<br>
+      <div  class=\"banner\">
+       <div class=\"inline-block\" style=\"display: inline-block;\">
+    <img  class=\"img\" src =\"";
+        // line 50
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("assets/images/car.png"), "html", null, true);
+        echo "\">
+    <img  class=\"img\" src =\"";
+        // line 51
+        echo twig_escape_filter($this->env, $this->env->getExtension('Symfony\Bridge\Twig\Extension\AssetExtension')->getAssetUrl("assets/images/longcar.png"), "html", null, true);
+        echo "\">
+     </div>
+    </div>
+<br>
+<div class=\"d-inline-flex mb-3\">
+       <div class=\"span2\">
+\t<p ><button id=\"valider\" class=\"genric-btn primary small \">valider</button></p>
+  </div>
+    </div>
+  </div>
+  <div id=\"response\">
+   <div id=\"html\"></div>         
+       <div class=\"block text-center\"><button id=\"ok\"  hidden=\"true\" class=\"genric-btn primary \">ok</button></div>
+  </div>
+ 
+  <!-- Tips Triks End -->
+
+  <script>
+    jQuery( document ).ready(function( \$ ) {
+      //identification de l'image sélectionné et trouver l'index de l'image de la bonne réponse
+        \$(\"#valider\").click(function () {
+          if(!\$('.img').hasClass('selectedd')){
+            alert(\"vide! choisir une option\");
+          }else{
+            if(\$('.selectedd').index() == 1){ 
+           \$(\"#html\").html('<div class=\"section-tittle text-center success\"><h2 style=\"color:green\">Bravo!</h2>\t');
+         document.getElementById('response').scrollIntoView();
+            }else{
+              \$(\"#html\").html('<div class=\"section-tittle text-center danger\"><h2 style=\"color:red\">Echec! la bonne réponse est la deuxième voiture </h2>\t');
+            document.getElementById('response').scrollIntoView();
+            }       
+             \$(this).prop(\"disabled\",true);
+             \$(\"#ok\").attr(\"hidden\",false);
+          }
+        });
+    
+ \$(\"#ok\").click(function () {
+          \$(\"#response\").text(\"\");
+        });
+    });
+
+\$(function(){
+  var ff;    
+    //pour  selectionner les images et implémenter le compteur
+   \$('.img').on('click', function(evt) {
+        ff = \$(this);        
+        \$('.img').removeClass('selectedd');
+        ff.addClass('selectedd');
+        evt.stopPropagation();
+        //alert(\"\"+ff.index());
+        
+    });   
+    
+});
+ 
+</script>
+
+
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+        
+        $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e->leave($__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@Gs/Default/grandeurs/comparerlongueurs.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  147 => 51,  143 => 50,  121 => 30,  112 => 29,  82 => 7,  73 => 6,  62 => 3,  53 => 2,  31 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("{% extends 'base.html.twig' %}
+{% block title %}
+  _ Comparer Longueurs
+{% endblock %}
+
+{% block main %}
+
+<style>
+.selectedd{
+border:2px inset silver;
+        }
+.small {
+    width: 92px !important;
+}
+.btn-lg{
+  width: 32px !important;
+}
+.inline-block{
+width: 300px;
+height: 150px;
+}
+img:hover {
+  opacity: 0.5;
+}
+</style>
+
+{% endblock %}
+
+{% block body %}
+  <!-- Tips Triks Start -->
+  <div class=\"tips-triks-area tips-padding\">
+    <div class=\"container\">
+      <!-- Section-tittle -->
+      <div class=\"row d-flex justify-content-center\">
+        <div class=\"col-lg-6 col-md-8 pr-0\">
+          <div class=\"section-tittle text-center\">
+            <h5>
+              Comparer Longueurs
+            </h5>
+          </div>
+        </div>
+      </div>
+      <br><br>
+    <div class=\"d-inline-flex mb-3\">
+    <p class=\"sample-text \">Quelle est le voiture <b>la plus longue</b>&nbsp;?<p>
+    </div>
+<br>
+      <div  class=\"banner\">
+       <div class=\"inline-block\" style=\"display: inline-block;\">
+    <img  class=\"img\" src =\"{{ asset('assets/images/car.png') }}\">
+    <img  class=\"img\" src =\"{{ asset('assets/images/longcar.png') }}\">
+     </div>
+    </div>
+<br>
+<div class=\"d-inline-flex mb-3\">
+       <div class=\"span2\">
+\t<p ><button id=\"valider\" class=\"genric-btn primary small \">valider</button></p>
+  </div>
+    </div>
+  </div>
+  <div id=\"response\">
+   <div id=\"html\"></div>         
+       <div class=\"block text-center\"><button id=\"ok\"  hidden=\"true\" class=\"genric-btn primary \">ok</button></div>
+  </div>
+ 
+  <!-- Tips Triks End -->
+
+  <script>
+    jQuery( document ).ready(function( \$ ) {
+      //identification de l'image sélectionné et trouver l'index de l'image de la bonne réponse
+        \$(\"#valider\").click(function () {
+          if(!\$('.img').hasClass('selectedd')){
+            alert(\"vide! choisir une option\");
+          }else{
+            if(\$('.selectedd').index() == 1){ 
+           \$(\"#html\").html('<div class=\"section-tittle text-center success\"><h2 style=\"color:green\">Bravo!</h2>\t');
+         document.getElementById('response').scrollIntoView();
+            }else{
+              \$(\"#html\").html('<div class=\"section-tittle text-center danger\"><h2 style=\"color:red\">Echec! la bonne réponse est la deuxième voiture </h2>\t');
+            document.getElementById('response').scrollIntoView();
+            }       
+             \$(this).prop(\"disabled\",true);
+             \$(\"#ok\").attr(\"hidden\",false);
+          }
+        });
+    
+ \$(\"#ok\").click(function () {
+          \$(\"#response\").text(\"\");
+        });
+    });
+
+\$(function(){
+  var ff;    
+    //pour  selectionner les images et implémenter le compteur
+   \$('.img').on('click', function(evt) {
+        ff = \$(this);        
+        \$('.img').removeClass('selectedd');
+        ff.addClass('selectedd');
+        evt.stopPropagation();
+        //alert(\"\"+ff.index());
+        
+    });   
+    
+});
+ 
+</script>
+
+
+{% endblock %}", "@Gs/Default/grandeurs/comparerlongueurs.html.twig", "C:\\wamp64\\www\\PFA\\src\\GsBundle\\Resources\\views\\Default\\grandeurs\\comparerlongueurs.html.twig");
+    }
+}
